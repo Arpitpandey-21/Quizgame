0827e9cd74560e98b7a540538990cc6e6f2be664
Arpit <arpitpandit892855@gmail.com>
2026-03-03 23:35:15 +0530

    2

--- lib/data/question.dart
+++ lib/data/question.dart
@@ -1,5 +1,94 @@
-import 'question_quiz.dart';
+import 'package:quiz_game/models/question_quiz.dart';
 
 const question = [
-
-]
+  QuizQuestion(
+    '💀 Duniya ka sabse zyada serial killers wala country kaunsa maana jata hai?',
+    [
+      'USA',
+      'Russia',
+      'India',
+      'Brazil',
+    ],
+  ),
+  QuizQuestion(
+    '🧠 Human brain death ke baad kitne minute tak active reh sakta hai?',
+    [
+      '2–3 minutes',
+      '1 minute',
+      '5–7 minutes',
+      '10+ minutes',
+    ],
+  ),
+  QuizQuestion(
+    '⚰ Kaunsa desh aisa hai jahan log kabron ke saath rehte hain (cemetery me ghar banake)?',
+    [
+      'Philippines',
+      'Indonesia',
+      'Mexico',
+      'Brazil',
+    ],
+  ),
+  QuizQuestion(
+    '☢ Chernobyl disaster kis country me hua tha?',
+    [
+      'Ukraine',
+      'Russia',
+      'Poland',
+      'Germany',
+    ],
+  ),
+  QuizQuestion(
+    '🧬 Human body ka kaunsa part death ke baad sabse last me decompose hota hai?',
+    [
+      'Teeth',
+      'Skin',
+      'Brain',
+      'Eyes',
+    ],
+  ),
+  QuizQuestion(
+    '🏚 India ka kaunsa place officially “Most Haunted” mana jata hai?',
+    [
+      'Bhangarh Fort',
+      'Shaniwar Wada',
+      'Kuldhara Village',
+      'Dumas Beach',
+    ],
+  ),
+  QuizQuestion(
+    '🌊 Titanic kis saal dubi thi?',
+    [
+      '1912',
+      '1905',
+      '1920',
+      '1898',
+    ],
+  ),
+  QuizQuestion(
+    '🧫 Har saal duniya me kitne log mosquito ke kaatne se marte hain approx?',
+    [
+      '700,000+',
+      '10,000',
+      '50,000',
+      '200,000',
+    ],
+  ),
+  QuizQuestion(
+    '🏥 Human body me kitne bacteria cells approx hote hain?',
+    [
+      '39 trillion approx',
+      '1 billion',
+      '10 billion',
+      '100 trillion',
+    ],
+  ),
+  QuizQuestion(
+    '🔪 World War 2 me approx kitne log mare gaye the?',
+    [
+      '70–85 million',
+      '10 million',
+      '30 million',
+      '50 million',
+    ],
+  ),
+];
--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -12,10 +12,29 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Questions Screen',
-        style: TextStyle(color: Colors.white, fontSize: 24),
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        const Text('THE QUESTION...', style: TextStyle(color: Colors.white,
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+        ),),
+        const SizedBox(height: 30),
+        ElevatedButton(onPressed: () {}, child: Text('Answer 1'),
+        ),
+        SizedBox(height:10),
+        ElevatedButton(onPressed: () {}, child: Text('Answer 2'),
+        ),
+        SizedBox(height:10),
+        ElevatedButton(onPressed: () {}, child: Text('Answer 3'),
+        ),
+        SizedBox(height:10),
+        ElevatedButton(onPressed: () {}, child: Text('Answer 4'),
+        ),
+        SizedBox(height:10),
+      ],
       ),
     );
   }
